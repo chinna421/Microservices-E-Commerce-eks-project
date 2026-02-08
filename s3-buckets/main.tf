88e2e904400dca4c3ac3a460adf86f8f5aff6454
chinna421 <108382623+chinna421@users.noreply.github.com>
2026-02-08 13:01:00 +0530

    Rename S3 buckets and update tags

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "rajashekarthop1"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "rajashekarthop1"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "rajashekarthop2"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "rajashekarthop2"
     Environment = "dev"
   }
 }
